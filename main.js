cc1f7b1b184b2a26fd330659736bc1187a823e3e
qkwl1013 <qkwl1013@naver.com>
2020-11-15 18:56:15 +0900

    JS : navbar menu click

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+//Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight= navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () =>{
@@ -8,4 +10,18 @@ document.addEventListener('scroll', () =>{
     }else{
         navbar.classList.remove('navbar--dark');
     }
+});
+
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',()=>{
+    const target = event.target;
+    const link  = target.dataset.link;
+    if(link == null){
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior:"smooth"});
 });
