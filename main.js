9b9a8218f0a6f4462fbc95c1951a39f00c4fdca0
qkwl1013 <qkwl1013@naver.com>
2020-11-17 01:05:17 +0900

    JS : arrow up

--- main.js
+++ main.js
@@ -31,11 +31,26 @@ contactme.addEventListener('click', (sc)=>{
 })
 
 
-//Make hime slowly fade to transparent as the window scrolls down
+//Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll',()=>{
      home.style.opacity=1-window.scrollY/homeHeight;
+});
+
+//Make arrow button when you click this button, we can go up
+const arobtn = document.querySelector(".arrow__btn");
+document.addEventListener('scroll',() =>{
+    if(homeHeight/2<window.scrollY){
+        // arobtn.style.display='inline-block';
+        arobtn.classList.add('visible');
+    }
+    else{
+        arobtn.classList.remove('visible');
+    }
+});
+const arobtnHome = arobtn.addEventListener('click',()=>{
+    scrollIntoViews("#home");
 });
 
 
